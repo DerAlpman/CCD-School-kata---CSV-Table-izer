93322a9cd96102c336033a7df43250efe515e958
Stefan Alpers <stefan.alpers@gmx.net>
2019-04-05 21:41:42 +0200

    kata finished

---
+++ csv_tableizer/CSVTableizer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace CSVTableizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvData = File.ReadAllLines("data.csv");
+
+            foreach (var item in Tablerizer.TablerizedCSVData(csvData))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csv_tableizer/CSVTableizer/Tablerizer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSVTableizer
+{
+    internal static class Tablerizer
+    {
+        internal static IEnumerable<string> TablerizedCSVData(string[] csvData)
+        {
+            int[] columnsWidths = Tablerizer.GetColumnsWidths(csvData);
+#
+            string headerUnderLine = BuildHeaderUnderline(columnsWidths);
+
+            var tablerizedCSVData = TablerizedRows(csvData, columnsWidths).ToArray();
+
+            for (int row = 0; row < csvData.Length; row++)
+            {
+                yield return tablerizedCSVData[row];
+                if (row == 0)
+                {
+                    yield return headerUnderLine;
+                }
+            }
+        }
+
+        private static IEnumerable<string> TablerizedRows(string[] csvData, int[] columnsWidths)
+        {
+
+            for (int row = 0; row < csvData.Length; row++)
+            {
+                var columns = csvData[row].Split(';');
+
+                StringBuilder rowString = new StringBuilder();
+                for (int column = 0; column < columns.Length; column++)
+                {
+                    rowString.Append(columns[column].PadRight(columnsWidths[column]) + '|');
+                }
+                yield return rowString.ToString();
+            }
+        }
+
+        private static string BuildHeaderUnderline(int[] columnsWidths)
+        {
+            StringBuilder headerUnderLine = new StringBuilder();
+            foreach (var columnWidth in columnsWidths)
+            {
+                headerUnderLine.Append(new string('-', columnWidth) + '+');
+            }
+
+            return headerUnderLine.ToString();
+        }
+
+        private static int[] GetColumnsWidths(string[] csvData)
+        {
+            int columnCount = csvData[0].Split(';').Count();
+
+            int[] columnsWidths = new int[columnCount];
+
+            for (int row = 0; row < csvData.Length; row++)
+            {
+                var columns = csvData[row].Split(';');
+
+                for (int column = 0; column < columns.Length; column++)
+                {
+                    columnsWidths[column] = Math.Max(columnsWidths[column], columns[column].Length);
+                }
+            }
+
+            return columnsWidths;
+        }
+    }
+}
